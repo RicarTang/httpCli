fdc095ca92ee3d4964fbf1ea262d32ce19f1c330
RicarTang <357965604@qq.com>
2023-11-21 15:03:54 +0800

    添加测试代码

--- tests/test_pyhttpcli.py
+++ tests/test_pyhttpcli.py
@@ -5,8 +5,29 @@ from pyhttpcli.main import app
 runner = CliRunner()
 
 
-def test_app():
-    result = runner.invoke(app, ["Camila", "--city", "Berlin"])
+def test_argument():
+    """测试必填参数"""
+    result = runner.invoke(app, ["http://api.tanghaibing.cn/testsuite/getAll"])
     assert result.exit_code == 0
-    assert "Hello Camila" in result.stdout
-    assert "Let's have a coffee in Berlin" in result.stdout
+    assert "200" in result.stdout
+    assert "'success': True" in result.stdout
+
+
+def test_option():
+    """测试基本可选项"""
+    result = runner.invoke(
+        app,
+        [
+            "http://api.tanghaibing.cn/user/login",
+            "-m",
+            "POST",
+            "-j",
+            "{'username':'admin','password':'123456'}",
+            "-v",
+        ],
+    )
+    assert result.exit_code == 0
+    assert "200" in result.stdout
+    assert "'success': True" in result.stdout
+    assert "Request Headers:" in result.stdout
+    assert "'username': 'admin'" in result.stdout
